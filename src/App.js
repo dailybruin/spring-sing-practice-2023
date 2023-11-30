394f460aa6cf28b29ad4e64edb18b13710fda6ec
Nicole Ju <nicoleju03@gmail.com>
2023-11-29 16:00:56 -0800

    update app.js to include aml

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ function App() {
   const [ data, setData ] = useState(null);
   
   useEffect(() => {
-		fetch("<TODO: insert api url here>")
-		.then(res => res.json())
-		.then(res => setData(res.data['article.aml']))
-  }, [])
+    fetch(
+      "https://kerckhoff.dailybruin.com/api/packages/flatpages/spring-sing-2023"
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res.data["article.aml"]));
+  }, []);
 
   return data && (
     <div className="App">
